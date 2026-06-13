04a95c7a751a94099b9bf1c5849b2f3f9b101370
Mohanad20100 <mohanad.mahmoud20100@gmail.com>
2026-06-13 19:47:38 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // تأكد أن المسار هنا صحيح
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
